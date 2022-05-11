d4e154d5a23695405eccf8fcd5e9b7ea688c02ce
tpquang <phuongquang4900@gmail.com>
2022-05-11 18:11:14 +0700

    http request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5001/api/users/')
+    .subscribe(
+      response => {this.users = response;}, // success callback
+      error => {console.log(error);} // error callback  
+    )
+  };
 }
