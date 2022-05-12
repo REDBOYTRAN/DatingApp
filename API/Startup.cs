3fcc131e681e6970c50630fbd42194286bcc755a
tpquang <phuongquang4900@gmail.com>
2022-05-12 18:09:49 +0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,7 +40,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
