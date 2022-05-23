2701e329bbb25590b4ee39df91620835dae4933a
redboytran <phuongquang4900@gmail.com>
2022-05-24 00:13:24 +0700

    update and create controllers

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            //provide the hash algorithm to create the passwordHash
+            using var hmac = new HMACSHA256();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
