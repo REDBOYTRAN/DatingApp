ea66270324e8cdef4c29a9ff5487c26a9d1ea1e5
Quang Tran <phuongquang4900@gmail.com>
2024-04-08 17:43:19 +0700

    End of section 11

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -106,5 +106,27 @@ namespace API.Controllers
 
             return BadRequest("Failed to set main photo");
         }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId) {
+            var user = await _userRepository.GetUserByUserNameAsync(User.GetUsername());
+            
+            var photoToDelete = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photoToDelete == null) return NotFound();
+
+            if (photoToDelete.IsMain) return BadRequest("You can not delete your main photo");
+            
+            if (photoToDelete.PublicId != null) {
+                var result = await _photoService.DeletePhotoAsync(photoToDelete.PublicId);
+                if(result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photoToDelete);
+
+            if(await _userRepository.SaveAllAsync()) return Ok();
+            
+            return BadRequest("Failed to delete photo");
+        }
     }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -50,15 +50,13 @@ export class PhotoEditorComponent {
     )
   }
 
-  // deletePhoto(photoId: number) {
-  //   this.memberService.deletePhoto(photoId).subscribe({
-  //     next: _ => {
-  //       if (this.member) {
-  //         this.member.photos = this.member?.photos.filter(x => x.id !== photoId)
-  //       }
-  //     }
-  //   })
-  // }
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      if(this.member) {
+        this.member.photos = this.member?.photos.filter(p => p.id !== photoId);
+      }
+    })
+  }
 
   initializeUploader() {
     this.uploader = new FileUploader({
