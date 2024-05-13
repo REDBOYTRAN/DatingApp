e68622e75088836eac7e45c2b114761500587783
QuangTran <phuongquang4900@gmail.com>
2024-05-13 23:19:42 +0700

    [SS14] Add Like Entity and Repository

---
+++ API/Data/LikeRepository.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Data
+{
+    public class LikeRepository : ILikesRepository
+    {
+        public Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserWithLikes(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
