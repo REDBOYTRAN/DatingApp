d05b6ed2d87980f6d742ef3b94a1d748d851d249
Quang Tran <phuongquang4900@gmail.com>
2024-07-18 15:29:21 +0700

    [SS15] Add messages UI

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '../_models/message';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +11,12 @@ export class MessageService {
 
   baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMessage(pageNumber: number, pageSize: number, container: string) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http)
+  }
 
 }
