9011df1b0e6ffcd8ed1ca07e93f8d70c2faf92c0
QuangTran <phuongquang4900@gmail.com>
2024-08-28 01:21:00 +0700

    End of section 16

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { User } from 'src/app/_models/user';
 import { AdminService } from 'src/app/_services/admin.service';
+import { RolesModalComponent } from 'src/app/modals/roles-modal/roles-modal.component';
 
 @Component({
   selector: 'app-user-management',
@@ -9,10 +11,10 @@ import { AdminService } from 'src/app/_services/admin.service';
 })
 export class UserManagementComponent implements OnInit {
 
+  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
   users: User[] = [];
-  constructor(private adminService: AdminService) {
-    
-  }
+
+  constructor(private adminService: AdminService, private modalService: BsModalService) { }
   ngOnInit(): void {
     this.getUsersWithRoles();
   }
@@ -21,5 +23,53 @@ export class UserManagementComponent implements OnInit {
     this.adminService.getUsersWithRoles().subscribe(users => {
       this.users = users;
     })
+  }
+
+  openRolesModal(user: User) {
+    const config = {
+      class: 'modal-dialog-centered',
+      initialState: {
+        user,
+        roles: this.getRolesArray(user)
+      }
+    }
+    this.bsModalRef = this.modalService.show(RolesModalComponent, config);
+    this.bsModalRef.content.updateSelectedRoles.subscribe(values => {
+      const rolesToUpdate = {
+        roles: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+      if (rolesToUpdate) {
+        this.adminService.updateUserRoles(user.username, rolesToUpdate.roles).subscribe(() => {
+          user.roles = [...rolesToUpdate.roles]
+        })
+      }
+    })
+  }
+
+  private getRolesArray(user) {
+    const roles = [];
+    const userRoles = user.roles;
+    const availableRoles: any[] = [
+      {name: 'Admin', value: 'Admin'},
+      {name: 'Moderator', value: 'Moderator'},
+      {name: 'Member', value: 'Member'}
+    ];
+
+    availableRoles.forEach(role => {
+      let isMatch = false;
+      for (const userRole of userRoles) {
+        if (role.name === userRole) {
+          isMatch = true;
+          role.checked = true;
+          roles.push(role);
+          break;
+        }
+      }
+      if (!isMatch) {
+        role.checked = false;
+        roles.push(role);
+      }
+    })
+    return roles;
   }
 }
