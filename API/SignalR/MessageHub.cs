f18bec6fb3bdf7448d10e1030b54b403341bb4fd
Quang Tran <phuongquang4900@gmail.com>
2025-03-12 16:35:01 +0700

    End of section 17

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -35,15 +35,18 @@ namespace API.SignalR
             var otherUser = httpContext.Request.Query["user"];
             var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await AddToGroup(groupName);
+            var group = await AddToGroup(groupName);
+
+            await Clients.Group(groupName).SendAsync("UpdatedGroup", group);
 
             var message = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
-            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", message);
+            await Clients.Caller.SendAsync("ReceiveMessageThread", message);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await RemoveFromMessageGroup();
+            var group = await RemoveFromMessageGroup();
+            await Clients.Group(group.Name).SendAsync("UpdatedGroup");
             await base.OnDisconnectedAsync(exception);
         }
 
@@ -102,7 +105,7 @@ namespace API.SignalR
 
         }
 
-        private async Task<bool> AddToGroup(string groupName)
+        private async Task<Group> AddToGroup(string groupName)
         {
             var group = await _messageRepository.GetMessageGroup(groupName);
             var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
@@ -115,14 +118,21 @@ namespace API.SignalR
 
             group.Connections.Add(connection);
 
-            return await _messageRepository.SaveAllAsync();
-        }
+            if (await _messageRepository.SaveAllAsync()) return group;
 
-        private async Task RemoveFromMessageGroup()
+            throw new HubException("Failed to add to group");
+        }
+ 
+        private async Task<Group> RemoveFromMessageGroup()
         {
-            var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+            var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+            var connection = group.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+    
             _messageRepository.RemoveConnection(connection);
-            await _messageRepository.SaveAllAsync();
+    
+            if (await _messageRepository.SaveAllAsync()) return group;
+    
+            throw new HubException("Failed to remove from group");
         }
     }
 }
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -7,6 +7,7 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { User } from '../_models/user';
 import { BehaviorSubject, take } from 'rxjs';
 import { error } from 'console';
+import { Group } from '../_models/group';
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +34,19 @@ export class MessageService {
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
+    })
+
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if (group.connections.some(x => x.username === otherUsername)) {
+        this.messageThread$.pipe(take(1)).subscribe(messages => {
+          messages.forEach(message => {
+            if (!message.dateRead) {
+              message.dateRead = new Date(Date.now())
+            }
+          })
+          this.messageThreadSource.next([...messages]);
+        })
+      }
     })
 
     this.hubConnection.on('NewMessage', message => {
